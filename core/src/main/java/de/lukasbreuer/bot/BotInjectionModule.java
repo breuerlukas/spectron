61a9998555a13a50c3844b72c2e56b7a6a4c3e59
DerCoderLukas <lukas.dercoder@gmail.com>
2023-04-24 15:11:27 +0200

    Renamed BotModule to BotInjectionModule

---
+++ core/src/main/java/de/lukasbreuer/bot/BotInjectionModule.java
@@ -0,0 +1,9 @@
+package de.lukasbreuer.bot;
+
+  import com.google.inject.AbstractModule;
+  import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(staticName = "create")
+public class BotInjectionModule extends AbstractModule {
+
+}
