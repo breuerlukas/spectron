7d4b9f024fd2e73636d2a492075f0e39b9b87e06
DerCoderLukas <lukas.dercoder@gmail.com>
2023-04-24 14:09:36 +0200

    Implemented Log system

---
+++ core/src/main/java/de/lukasbreuer/bot/log/Log.java
@@ -0,0 +1,70 @@
+package de.lukasbreuer.bot.log;
+
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.*;
+
+public final class Log extends Logger {
+  public static Log create(String name, String path) throws Exception {
+    var consoleHandler = new ConsoleHandler();
+    consoleHandler.setFormatter(LogFormat.create(LogFormat.FormatType.CONSOLE));
+    consoleHandler.setLevel(Level.ALL);
+    var fileHandler = new FileHandler(buildLogFilePath(path));
+    fileHandler.setFormatter(LogFormat.create(LogFormat.FormatType.FILE));
+    var log = new Log(name, consoleHandler, fileHandler);
+    log.setLevel(Level.ALL);
+    log.addHandler(consoleHandler);
+    log.addHandler(fileHandler);
+    Runtime.getRuntime().addShutdownHook(new Thread(log::close));
+    return log;
+  }
+
+  private static String buildLogFilePath(String basePath) {
+    var logPath = System.getProperty("user.dir") + basePath +
+      new SimpleDateFormat("yyyy-MM-dd-HHmmss").format(new Date()) + ".log";
+    var logFile = new File(logPath);
+    if (!logFile.getParentFile().exists()) {
+      logFile.getParentFile().mkdirs();
+    }
+    return logPath;
+  }
+
+  private final ConsoleHandler consoleHandler;
+  private final FileHandler fileHandler;
+
+  private Log(String name, ConsoleHandler consoleHandler, FileHandler fileHandler) {
+    super(name, null);
+    this.consoleHandler = consoleHandler;
+    this.fileHandler = fileHandler;
+  }
+
+  @Override
+  public void log(LogRecord record) {
+    super.log(formatRecord(record));
+  }
+
+  public void consoleLog(Level level, String message) {
+    restrictedLog(consoleHandler, new LogRecord(level, message));
+  }
+
+  public void fileLog(Level level, String message) {
+    restrictedLog(fileHandler, new LogRecord(level, message));
+  }
+
+  private void restrictedLog(Handler handler, LogRecord record) {
+    handler.publish(formatRecord(record));
+  }
+
+  private LogRecord formatRecord(LogRecord record) {
+    record.setMessage("[" + getName().toUpperCase() + "] " + record.getMessage());
+    return record;
+  }
+
+  public void close() {
+    for (var handler : getHandlers()) {
+      handler.close();
+    }
+  }
+}
---
+++ core/src/main/java/de/lukasbreuer/bot/log/LogFormat.java
@@ -0,0 +1,73 @@
+package de.lukasbreuer.bot.log;
+
+import lombok.RequiredArgsConstructor;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+@RequiredArgsConstructor(staticName = "create")
+public final class LogFormat extends Formatter {
+  public enum FormatType {
+    CONSOLE,
+    FILE;
+
+    public boolean isConsole() {
+      return this == CONSOLE;
+    }
+
+    public boolean isFile() {
+      return this == FILE;
+    }
+  }
+
+  private final FormatType formatType;
+  private final DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+  private static final String COLOR_RESET = "\u001B[0m";
+
+  @Override
+  public String format(LogRecord record) {
+    var result = new StringBuilder();
+    if (formatType.isConsole()) {
+      result.append(determineColor(record));
+    }
+    result.append(buildPrefix(record));
+    result.append(formatMessage(record));
+    if (formatType.isConsole()) {
+      result.append(COLOR_RESET);
+    }
+    result.append("\n");
+    return result.toString();
+  }
+
+  private static final String COLOR_WHITE = "\033[0;97m";
+  private static final String COLOR_RED = "\033[0;91m";
+  private static final String COLOR_DARK_RED = "\033[0;31m";
+  private static final String COLOR_CYAN = "\033[0;36m";
+
+  private String determineColor(LogRecord record) {
+    if (record.getLevel() == Level.WARNING) {
+      return COLOR_RED;
+    }
+    if (record.getLevel() == Level.SEVERE) {
+      return COLOR_DARK_RED;
+    }
+    if (record.getLevel() == Level.FINE) {
+      return COLOR_CYAN;
+    }
+    return COLOR_WHITE;
+  }
+
+  private String buildPrefix(LogRecord record) {
+    var prefix = new StringBuilder();
+    prefix.append("[");
+    prefix.append(dateFormat.format(record.getMillis()));
+    prefix.append(" - ");
+    prefix.append(record.getLevel().getLocalizedName());
+    prefix.append("]: ");
+    return prefix.toString();
+  }
+}
