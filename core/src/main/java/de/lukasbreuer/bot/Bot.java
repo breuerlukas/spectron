7a8a2d8aebd7016fddf6b58d137575f4f65802fe
DerCoderLukas <lukas.dercoder@gmail.com>
2023-04-24 14:09:55 +0200

    Updated Bot & BotApplication

--- core/src/main/java/de/lukasbreuer/bot/Bot.java
+++ core/src/main/java/de/lukasbreuer/bot/Bot.java
@@ -1,15 +1,21 @@
 package de.lukasbreuer.bot;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import de.lukasbreuer.bot.authentication.Authentication;
+import de.lukasbreuer.bot.authentication.Credentials;
 import de.lukasbreuer.bot.connection.ConnectionClient;
 import de.lukasbreuer.bot.connection.packet.PacketRegistry;
-import de.lukasbreuer.bot.connection.packet.inbound.login.PacketDisconnect;
 import de.lukasbreuer.bot.connection.packet.inbound.login.PacketEncryptionRequest;
+import de.lukasbreuer.bot.connection.packet.inbound.login.PacketLoginDisconnect;
 import de.lukasbreuer.bot.connection.packet.inbound.login.PacketLoginSuccess;
 import de.lukasbreuer.bot.connection.packet.inbound.login.PacketSetCompression;
 import de.lukasbreuer.bot.connection.packet.outbound.handshake.PacketHandshake;
 import de.lukasbreuer.bot.connection.packet.outbound.login.PacketEncryptionResponse;
 import de.lukasbreuer.bot.connection.packet.outbound.login.PacketLoginStart;
 import de.lukasbreuer.bot.connection.packet.outbound.play.PacketChatMessage;
+import de.lukasbreuer.bot.event.EventExecutor;
+import de.lukasbreuer.bot.log.Log;
 import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
@@ -18,27 +24,45 @@ import java.util.UUID;
 public final class Bot {
   private final String hostname;
   private final short port;
+  private final Credentials credentials;
+  private final UUID uuid;
+  private Injector injector;
+  private Log log;
+  private Authentication authentication;
   private ConnectionClient connection;
 
+  public void initialize() throws Exception {
+    injector = Guice.createInjector(BotInjectionModule.create());
+    log = Log.create("Core", "/logs/core/");
+    authentication = Authentication.create(credentials, uuid);
+  }
+
   public void connect() throws Exception {
-    connection = ConnectionClient.create(registerPackets(), hostname, port);
+    connection = ConnectionClient.create(registerPackets(), hostname, port,
+      injector.getInstance(EventExecutor.class));
     connection.connectAsync(() -> {
-      connection.sendPacket(new PacketHandshake(762, hostname, port));
-      connection.sendPacket(new PacketLoginStart("Lynoo",
-        UUID.fromString("0ffbc827-0a24-4890-a3a4-ea7e19dd1a14")));
+
     });
   }
 
   private PacketRegistry registerPackets() throws Exception {
-    var registry = PacketRegistry.create();
+    var registry = injector.getInstance(PacketRegistry.class);
+    registerOutgoingPackets(registry);
+    registerIncomingPackets(registry);
+    return registry;
+  }
+
+  private void registerOutgoingPackets(PacketRegistry registry) throws Exception {
     registry.registerOutgoingPacket(PacketHandshake.class);
     registry.registerOutgoingPacket(PacketLoginStart.class);
     registry.registerOutgoingPacket(PacketEncryptionResponse.class);
     registry.registerOutgoingPacket(PacketChatMessage.class);
-    registry.registerIncomingPacket(PacketDisconnect.class);
+  }
+
+  private void registerIncomingPackets(PacketRegistry registry) throws Exception {
+    registry.registerIncomingPacket(PacketLoginDisconnect.class);
     registry.registerIncomingPacket(PacketEncryptionRequest.class);
     registry.registerIncomingPacket(PacketSetCompression.class);
     registry.registerIncomingPacket(PacketLoginSuccess.class);
-    return registry;
   }
 }
--- core/src/main/java/de/lukasbreuer/bot/BotApplication.java
+++ core/src/main/java/de/lukasbreuer/bot/BotApplication.java
@@ -1,7 +1,14 @@
 package de.lukasbreuer.bot;
 
+import de.lukasbreuer.bot.authentication.Credentials;
+
+import java.util.UUID;
+
 public final class BotApplication {
   public static void main(String[] args) throws Exception {
-    Bot.create("localhost", (short) 25565).connect();
+    var bot = Bot.create("localhost", (short) 25565, Credentials.create("lukas.breuer2004@gmail.com",
+      "lukas2004510tilobreuer"), UUID.fromString("0ffbc827-0a24-4890-a3a4-ea7e19dd1a14"));
+    bot.initialize();
+    bot.connect();
   }
 }
