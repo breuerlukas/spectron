e114370586d3862e1c59f6b873cef46674223025
DerCoderLukas <lukas.dercoder@gmail.com>
2023-05-06 12:02:17 +0200

    Implemented ConnectionConfiguration & ConnectionInjectionModule

---
+++ core/src/main/java/de/lukasbreuer/bot/connection/ConnectionInjectionModule.java
@@ -0,0 +1,37 @@
+package de.lukasbreuer.bot.connection;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(staticName = "create")
+public final class ConnectionInjectionModule extends AbstractModule {
+  @Provides
+  @Singleton
+  ConnectionConfiguration provideConnectionConfiguration() throws Exception {
+    return ConnectionConfiguration.createAndLoad();
+  }
+
+  @Provides
+  @Singleton
+  @Named("hostname")
+  String provideHostname(ConnectionConfiguration configuration) {
+    return configuration.hostname();
+  }
+
+  @Provides
+  @Singleton
+  @Named("port")
+  short providePort(ConnectionConfiguration configuration) {
+    return configuration.port();
+  }
+
+  @Provides
+  @Singleton
+  @Named("protocolVersion")
+  int provideProtocolVersion(ConnectionConfiguration configuration) {
+    return configuration.protocolVersion();
+  }
+}
