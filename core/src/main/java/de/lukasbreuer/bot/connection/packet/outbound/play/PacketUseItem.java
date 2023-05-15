067a7c23ec1dd024563fdba006551ceecbb019a6
DerCoderLukas <lukas.dercoder@gmail.com>
2023-05-15 18:27:59 +0200

    Implemented GriefergamesBotRefill

---
+++ core/src/main/java/de/lukasbreuer/bot/connection/packet/outbound/play/PacketUseItem.java
@@ -0,0 +1,48 @@
+package de.lukasbreuer.bot.connection.packet.outbound.play;
+
+import de.lukasbreuer.bot.block.BlockFace;
+import de.lukasbreuer.bot.block.BlockPosition;
+import de.lukasbreuer.bot.connection.ProtocolState;
+import de.lukasbreuer.bot.connection.packet.PacketBuffer;
+import de.lukasbreuer.bot.connection.packet.outbound.PacketOutgoing;
+
+public final class PacketUseItem extends PacketOutgoing {
+  private final BlockPosition blockPosition;
+  private final BlockFace blockFace;
+  private final float cursorX;
+  private final float cursorY;
+  private final float cursorZ;
+
+  public PacketUseItem(
+    BlockPosition blockPosition, BlockFace blockFace, float cursorX,
+    float cursorY, float cursorZ
+  ) {
+    super(0x31, ProtocolState.PLAY);
+    this.blockPosition = blockPosition;
+    this.blockFace = blockFace;
+    this.cursorX = cursorX;
+    this.cursorY = cursorY;
+    this.cursorZ = cursorZ;
+  }
+
+  public PacketUseItem() {
+    super(0x31, ProtocolState.PLAY);
+    this.blockPosition = null;
+    this.blockFace = null;
+    this.cursorX = -1;
+    this.cursorY = -1;
+    this.cursorZ = -1;
+  }
+
+  @Override
+  public void write(PacketBuffer buffer) {
+    buffer.writeVarInt(0);
+    buffer.writeBlockPosition(blockPosition);
+    buffer.writeVarInt(blockFace.value());
+    buffer.raw().writeFloat(cursorX);
+    buffer.raw().writeFloat(cursorY);
+    buffer.raw().writeFloat(cursorZ);
+    buffer.raw().writeBoolean(false);
+    buffer.writeVarInt(0);
+  }
+}
