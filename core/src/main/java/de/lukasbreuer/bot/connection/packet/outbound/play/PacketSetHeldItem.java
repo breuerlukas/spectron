4156d96b83c2dbbcef3861b4b8e5d96c7ae0ae29
DerCoderLukas <lukas.dercoder@gmail.com>
2023-05-15 11:23:25 +0200

    Added PacketSetHeldItem

---
+++ core/src/main/java/de/lukasbreuer/bot/connection/packet/outbound/play/PacketSetHeldItem.java
@@ -0,0 +1,24 @@
+package de.lukasbreuer.bot.connection.packet.outbound.play;
+
+import de.lukasbreuer.bot.connection.ProtocolState;
+import de.lukasbreuer.bot.connection.packet.PacketBuffer;
+import de.lukasbreuer.bot.connection.packet.outbound.PacketOutgoing;
+
+public final class PacketSetHeldItem extends PacketOutgoing {
+  private final short slot;
+
+  public PacketSetHeldItem(short slot) {
+    super(0x28, ProtocolState.PLAY);
+    this.slot = slot;
+  }
+
+  public PacketSetHeldItem() {
+    super(0x28, ProtocolState.PLAY);
+    this.slot = -1;
+  }
+
+  @Override
+  public void write(PacketBuffer buffer) {
+    buffer.raw().writeShort(slot);
+  }
+}
